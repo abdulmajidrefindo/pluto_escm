f345d4af9e1d46a665dee3a23ab4ab4d0f3b59a3
Hernando Dio Palma <hernando.diopalma@gmail.com>
2022-12-08 18:03:37 +0700

    Kombinasi data transaksi untuk Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\TransaksiPemasok;
 use App\Models\TransaksiPelanggan;
 use App\Models\Pelanggan;
+use App\Models\Pemasok;
 use App\Models\Barang;
+use Illuminate\Support\Facades\DB;
 
 
 use Illuminate\Http\Request;
@@ -29,6 +31,20 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        // Kombinasi dua tabel untuk mendapat data transaksi terbaru
+        $transaksiPemasok = TransaksiPemasok::join('pemasok','transaksi_pemasok.pemasok_id','=','pemasok.id')
+                                ->select(DB::raw('transaksi_pemasok.id,pemasok.nama_pemasok as nama,transaksi_pemasok.total_harga,transaksi_pemasok.created_at, "Masuk" as jenis_transaksi'));
+        $transaksiPelanggan = TransaksiPelanggan::join('pelanggan','transaksi_pelanggan.pelanggan_id','=','pelanggan.id')
+                                ->select(DB::raw('transaksi_pelanggan.id,pelanggan.nama_pelanggan as nama,transaksi_pelanggan.total_harga,transaksi_pelanggan.created_at, "Keluar" as jenis_transaksi'));
+        $transaksiTerbaru = $transaksiPelanggan->unionAll($transaksiPemasok)->orderBy('created_at', 'desc')->take(7)->get();
+
+        //Hitung jumlah data
+
+
+        return response()->json($transaksiTerbaru);
+        //return view('index');
+
+
     }
 }
