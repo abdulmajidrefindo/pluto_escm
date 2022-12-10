5ca90c7eb67bdc349568556fbb0e0f0b5c2ee947
Ismail <3332190037@untirta.ac.id>
2022-12-10 14:58:32 +0700

    Nambah library sama updet view

--- resources/views/kategori/show.blade.php
+++ resources/views/kategori/show.blade.php
@@ -54,6 +54,48 @@
     </table>
   </div>
 </div>
+
+<div>
+    <h4>Daftar Produk dengan Kategori {{$kategori->nama_kategori}}</h4>
+</div>
+
+<div class="card">
+    <div class="card-body">
+        <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+        <table id="myTable" class="table">
+            <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>Umur</th>
+                    <th>Alamat</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Rizki</td>
+                    <td>23</td>
+                    <td>Jakarta</td>
+                </tr>
+                <tr>
+                    <td>Adi</td>
+                    <td>25</td>
+                    <td>Bogor</td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <div class="text-center">
+            <ul class="pagination pagination-centered">
+                <li><a href="#" style="color: black; float: left; padding: 8px 16px; text-decoration: none; transition: background-color .3s; border: 1px solid #ddd; margin: 0 4px;">1</a></li>
+                <li><a href="#" style="color: black; float: left; padding: 8px 16px; text-decoration: none; transition: background-color .3s; border: 1px solid #ddd; margin: 0 4px;">2</a></li>
+                <li><a href="#" style="color: black; float: left; padding: 8px 16px; text-decoration: none; transition: background-color .3s; border: 1px solid #ddd; margin: 0 4px;">3</a></li>
+                <li><a href="#" style="color: black; float: left; padding: 8px 16px; text-decoration: none; transition: background-color .3s; border: 1px solid #ddd; margin: 0 4px;">4</a></li>
+                <li><a href="#" style="color: black; float: left; padding: 8px 16px; text-decoration: none; transition: background-color .3s; border: 1px solid #ddd; margin: 0 4px;">5</a></li>
+            </ul>
+            <br>
+        </div>
+    </div>
+</div>
 <!-- /.card --
 
 
