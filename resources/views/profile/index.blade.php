780c152fa4aedb93bd5be43774687b1340b498f3
Ivan Munandar <3332190035@untirta.ac.id>
2022-12-14 17:36:39 +0700

    ganti warna header tabel

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,10 +1,11 @@
 @extends('adminlte::page')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card card-dark">
                 <div class="card-header">{{ __('Profile') }}</div>
 
                 <div class="card-body">
